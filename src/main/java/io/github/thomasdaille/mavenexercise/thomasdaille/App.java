16331d8638bdab43cf1087668596b87fdb1571e3
daillet <tom.daille@gmail.com>
2020-03-17 19:47:52 +0100

    maven exercise

---
+++ src/main/java/io/github/thomasdaille/mavenexercise/thomasdaille/App.java
@@ -0,0 +1,13 @@
+package io.github.thomasdaille.mavenexercise.thomasdaille;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
